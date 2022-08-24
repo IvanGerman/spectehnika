72d3bd4d8cc1fb31fb4c521951206195161436b6
IvanGerman <ivgvi@mail.ru>
2022-08-24 13:59:22 +0700

    feat: implementing logic for spectehnikaPage

--- src/pages/spectechnikaPage/SpectechnikaPage.js
+++ src/pages/spectechnikaPage/SpectechnikaPage.js
@@ -1,15 +1,18 @@
 //import { SamosvalLogicClass } from "../../components/uslugiComponents/UslugiLogicClass";
-import { currentKeyName, prices } from '../../state/data';
-import './spectechnikaPage.scss';
+import { currentKeyName, description, prices, sectionName, srcPath } from '../../state/data';
+import './SpectechnikaPage.scss';
 
 
 const SpectechnikaPage = {
   render: async () => {
     const view = `
       <div class="spectechnikaPage">
-        <h2>SpectechnikaPage</h2>
+        <h2>${sectionName[currentKeyName.currentKeyName]}</h2>
+        <br/><br/>
+        <img src="${srcPath[currentKeyName.currentKeyName]}" alt="" class="spectechImage"/>
         <br/><br/>
         <p class="spectechnikaPagePreis"></p>
+        <p class="spectechnikaDeskription">${description[currentKeyName.currentKeyName]}</p>
         <a href="#/">StartPage</a>
       </div>
     `;
