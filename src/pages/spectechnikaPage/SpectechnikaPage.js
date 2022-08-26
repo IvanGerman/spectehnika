de57c0f0dd0358198f88640cc33b5c2272c88667
IvanGerman <ivgvi@mail.ru>
2022-08-26 18:45:53 +0700

    feat: add css styles to spectehnika markup

--- src/pages/spectechnikaPage/SpectechnikaPage.js
+++ src/pages/spectechnikaPage/SpectechnikaPage.js
@@ -5,15 +5,31 @@ import './SpectechnikaPage.scss';
 
 const SpectechnikaPage = {
   render: async () => {
-    document.documentElement.scrollTop = document.body.scrollTop = 900;
+    document.documentElement.scrollTop = document.body.scrollTop = 950;
     const view = `
       <div class="spectechnikaPage">
         <h2>${sectionName[currentKeyName.currentKeyName]}</h2>
-        <br/><br/>
         <img src="${srcPath[currentKeyName.currentKeyName]}" alt="" class="spectechImage"/>
-        <br/><br/>
-        <p class="spectechnikaPagePreis"></p>
+        <div class="navlinks" >
+          <p class="navlinksP">Спецтехника:</p>
+          <a href="#/">gdgd</a>
+          <a href="#/">gdgd</a>
+          <a href="#/">gdgd</a>
+          <a href="#/">gdgd</a>
+          <a href="#/">gdgd</a>
+          <a href="#/">gdgd</a>
+          <a href="#/">gdgd</a>
+          <a href="#/">gdgd</a>
+          <a href="#/">gdgd</a>
+          <p class="navlinksP">Услуги:</p>
+          <a href="#/">gdgd</a>
+          <a href="#/">gdgd</a>
+          <a href="#/">gdgd</a>
+          <a href="#/">gdgd</a>
+        </div>
         <p class="spectechnikaDeskription">${description[currentKeyName.currentKeyName]}</p>
+        <p class="spectechnikaPagePreisP">От <span class="spectechnikaPagePreis">2500</span> рублей/час</p>
+      
         <a href="#/"><div class="backBtn">Назад</div></a>
       </div>
     `;
