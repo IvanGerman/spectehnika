ec2c7dfc2ce130734797b18ca94f9551a0f8c306
IvanGerman <68238341+IvanGerman@users.noreply.github.com>
2022-09-10 14:05:19 +0700

    Delete src directory

--- src/state/functions.js
+++
@@ -1,8 +0,0 @@
-export function changeUrl(hash) {
-  let url_ob = new URL(document.URL);
-  url_ob.hash = hash;
-  // new url
-  let new_url = url_ob.href;
-  // change the current url
-  document.location.href = new_url;
-}
